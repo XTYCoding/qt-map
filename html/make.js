a19932671377ea88c5db6eb65d89733a79dd9874
XTYCoding <3401849905@qq.com>
2022-12-12 17:54:30 +0800

    增加了标点 并且可以获取标点的信息

--- html/make.js
+++ html/make.js
@@ -1,48 +1,44 @@
-function draw(type){
-    switch(type){
-      case 'marker':{
-          mouseTool.marker({
-            icon: startIcon,
-            anchor: new AMap.Pixel(6, 6),
-          });
-          break;
-      }
-      case 'circle':{
-          mouseTool.circle({
-            fillColor:'#00b0ff',
-            strokeColor:'#80d8ff'
-            //同Circle的Option设置
-          });
-      }
-    }
-  }
+// function draw(type){
+//     switch(type){
+//       case 'marker':{
+//           mouseTool.marker({
+//             icon: startIcon,
+//             anchor: new AMap.Pixel(6, 6),
+//           });
+//           break;
+//       }
+//       case 'circle':{
+//           mouseTool.circle({
+//             fillColor:'#00b0ff',
+//             strokeColor:'#80d8ff'
+//             //同Circle的Option设置
+//           });
+//       }
+//     }
+//   }
 
-function makenodelist(e){
-    var lonlat = e.lnglat.getLng()+','+e.lnglat.getLat();
-    nodelist.push(lonlat); 
+
+function addNode(marker,nodeList){
+    var lnglat = marker.getPosition().getLng()+','+marker.getPosition().getLat();
+    nodeList.push(lnglat); 
 }
 
-function makelist(){
-    for(let i = 0;i<nodelist.length-1;i++){
-       var lonlat1 = [nodelist[i].split(",")[0],nodelist[i].split(",")[1]];
-       var lonlat2 = [nodelist[i+1].split(",")[0],nodelist[i+1].split(",")[1]];
-       var distance = AMap.GeometryUtil.distance(lonlat1, lonlat2);//计算两点间距离
-       nodelist[i]+=',';
-       nodelist[i]+=distance;
+function makelist(nodeList){
+    for(let i = 0;i<nodeList.length-1;i++){
+       var lnglat1 = [nodeList[i].split(",")[0],nodeList[i].split(",")[1]];
+       var lnglat2 = [nodeList[i+1].split(",")[0],nodeList[i+1].split(",")[1]];
+       var distance = AMap.GeometryUtil.distance(lnglat1, lnglat2);//计算两点间距离
+       nodeList[i]+=',';
+       nodeList[i]+=distance;
     }
 }
 
-var mark=function(){
-    //切换选项卡
-      document.getElementById("pointstylecard").style.display="none";
-      document.getElementById("circle").style.display="none";
-      document.getElementById("point").style.display="";
-      document.getElementById("drawcard").style.display="";
-      for(var i=0;i<radios.length;i+=1){
-          radios[i].onchange = function(e){
-            draw('marker')
-          }
-      }
-      draw('marker')
-      map.on('click', makenodelist);
-  }
+// var mark=function(){
+//       for(var i=0;i<radios.length;i+=1){
+//           radios[i].onchange = function(e){
+//             draw('marker')
+//           }
+//       }
+//       draw('marker')
+//       map.on('click', makenodelist);
+//   }
